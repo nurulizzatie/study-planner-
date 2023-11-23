97128624eeeda95b67786f80d0efe45a57fe0095
nurulizzatie <126445328+nurulizzatie@users.noreply.github.com>
2023-11-23 14:39:39 +0800

    Create StatsPage.java

---
+++ StatsPage.java
@@ -0,0 +1,6 @@
+public class StatsPage {
+    public void showStatsPage() {
+        System.out.println("\nStatistics:");
+        // Implement logic to display dummy data for charts and graphs
+    }
+}
